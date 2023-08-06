c5f1b9a3ee1cf4dc6217236f86fd0801e5c49f95
hspahic-cs <harrisspahic1190@gmail.com>
2023-08-06 15:00:51 -0400

    Adding some code to load in credentials

---
+++ creating-auth.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+dotenv_path = Path('./.env')
+
+load_dotenv(dotenv_path)
+
+def get_user(username):
+    if(username == "Saoirse"):
+        saoirse_id = os.getenv("SAOIRSE_ID", "not found")
+        saoirse_secret = os.getenv("SAOIRSE_SECRET", "not found")
+
+        print(saoirse_id)
+        
+        if saoirse_id and saoirse_secret:
+            os.environ['SPOTIPY_CLIENT_ID'] = saoirse_id
+            os.environ['SPOTIPY_CLIENT_SECRET'] = saoirse_secret
+        else:
+            print("SAOIRSE credentials not found in .env")
+
+    elif (username == "Harris"): 
+        os.environ['SPOTIPY_CLIENT_ID'] = os.getenv("HARRIS_ID")
+        os.environ['SPOTIPY_CLIENT_SECRET'] = os.getenv("HARRIS_SECRET")
+
+get_user("Saoirse")
