6a4ffaa49a02419c8536a953457fbcc60188a4e8
saoirsemooney <saoirse.mooney@gmail.com>
2023-08-10 18:06:42 -0400

    demo code from spotipy testing

--- creating-auth.py
+++ creating-auth.py
@@ -10,17 +10,16 @@ def get_user(username):
     if(username == "Saoirse"):
         saoirse_id = os.getenv("SAOIRSE_ID", "not found")
         saoirse_secret = os.getenv("SAOIRSE_SECRET", "not found")
-
-        print(saoirse_id)
         
         if saoirse_id and saoirse_secret:
             os.environ['SPOTIPY_CLIENT_ID'] = saoirse_id
             os.environ['SPOTIPY_CLIENT_SECRET'] = saoirse_secret
+            print(os.getenv('SPOTIPY_CLIENT_ID'))
         else:
             print("SAOIRSE credentials not found in .env")
 
     elif (username == "Harris"): 
-        os.environ['SPOTIPY_CLIENT_ID'] = os.getenv("HARRIS_ID")
-        os.environ['SPOTIPY_CLIENT_SECRET'] = os.getenv("HARRIS_SECRET")
+        os.environ['SPOTIPY_CLIENT_ID'] = os.getenv("HARRIS_ID", "not found")
+        os.environ['SPOTIPY_CLIENT_SECRET'] = os.getenv("HARRIS_SECRET", "not found")
 
-get_user("Saoirse")
+        print("Switching to Harris")
--- dataset-creation.py
+++ dataset-creation.py
@@ -1,16 +1,40 @@
 import spotipy
+import importlib
+import os
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
+creating_auth = importlib.import_module("creating-auth")
+creating_auth.get_user("Saoirse")
+# auth_manager = SpotifyClientCredentials()
+# sp = spotipy.Spotify(auth_manager=auth_manager)
 
+# playlists = sp.current_user_recently_played()
+# while playlists:
+#     for i, playlist in enumerate(playlists['items']):
+#         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+#     if playlists['next']:
+#         playlists = sp.next(playlists)
+#     else:
+#         playlists = None
+scope = "user-library-read"
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
+# playlist = spotify.playlist(playlist_id, fields=None, market=None, additional_types=('track', ))
 
-for album in albums:
-    print(album['name'])
+# results = spotify.artist_albums(birdy_uri, album_type='album')
+# albums = results['items']
+# while results['next']:
+#     results = spotify.next(results)
+#     albums.extend(results['items'])
+
+# for album in albums:
+#     print(album['name'])
+
+os.environ.pop("SPOTIPY_CLIENT_ID")
+os.environ.pop("SPOTIPY_CLIENT_SECRET")
